5cc4582ede7bb2e99cdcaea5f74af066c00a2de8
Czihi <boslak@student.agh.edu.pl>
2020-07-28 15:18:40 +0200

    Creating nike store app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/App.css';
+import {
+    BrowserRouter as Router,
+    Route
+} from 'react-router-dom';
+import nikeLogo from "./images/nikeLogo.png"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <Route path="/Nike-store" exact render={
+                    () => {
+                        return (
+                            <div className="App">
+                                <img className="nikeLogo" src={nikeLogo} alt="logo"/>
+                            </div>
+                        )
+                    }
+                }/>
+            </Router>
+        )
+    };
 }
 
 export default App;
