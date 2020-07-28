4efa8e2d0ff875d76f2f1e9df292d234f392df19
Czihi <boslak@student.agh.edu.pl>
2020-07-28 21:49:14 +0200

    Updates

---
+++ precache-manifest.36944ae165ba80283af95a9686d091e8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "b97a2eb958abb3ff64439b798bf4e491",
+    "url": "/Nike-store/static/media/greyDot.b97a2eb9.png"
+  },
+  {
+    "revision": "b5becaa87a3b052bd552",
+    "url": "/Nike-store/static/css/main.86da9a91.chunk.css"
+  },
+  {
+    "revision": "03962453c7bb61adc5cb",
+    "url": "/Nike-store/static/js/runtime~main.52bada7b.js"
+  },
+  {
+    "revision": "11c0d71db9e0a5e4bdd8098a63cff563",
+    "url": "/Nike-store/static/media/background.11c0d71d.png"
+  },
+  {
+    "revision": "eed1a97b0b2c35f945cd",
+    "url": "/Nike-store/static/js/2.107e28b0.chunk.js"
+  },
+  {
+    "revision": "a839c107580940ebf6ffcb47be099061",
+    "url": "/Nike-store/static/media/nikeLogo.a839c107.png"
+  },
+  {
+    "revision": "0b36f33ee04701c5c0caf9565db10eda",
+    "url": "/Nike-store/static/media/leftShoe.0b36f33e.png"
+  },
+  {
+    "revision": "b5becaa87a3b052bd552",
+    "url": "/Nike-store/static/js/main.d6a1b751.chunk.js"
+  },
+  {
+    "revision": "755191da33eed9d7930e61312db68926",
+    "url": "/Nike-store/static/media/blackDot.755191da.png"
+  },
+  {
+    "revision": "d93fa3162a38e01359eb904e989a16fb",
+    "url": "/Nike-store/static/media/nikeShoes1.d93fa316.png"
+  },
+  {
+    "revision": "76433905658a8c6bc4d1234afd2283dc",
+    "url": "/Nike-store/static/media/nikeShoes2.76433905.png"
+  },
+  {
+    "revision": "28d1e326780c1a95c519583767c2d887",
+    "url": "/Nike-store/static/media/nikeShoes3.28d1e326.png"
+  },
+  {
+    "revision": "566516de2288ca1950c6d527645798fb",
+    "url": "/Nike-store/static/media/cart.566516de.png"
+  },
+  {
+    "revision": "eed1a97b0b2c35f945cd",
+    "url": "/Nike-store/static/css/2.02787c02.chunk.css"
+  },
+  {
+    "revision": "88ab7e8a43c38c6aeaa34c1528fec1ce",
+    "url": "/Nike-store/index.html"
+  }
+];
